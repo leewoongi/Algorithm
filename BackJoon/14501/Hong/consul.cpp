075df9b2b260948804ac10944e41bdd48975e8b5
jonghyunH <adad357@naver.com>
2020-02-05 17:57:30 +0900

    solve #5 boj_14501

---
+++ BackJoon/14501/Hong/consul.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, start, sum;
+int consul[16][2];
+int dp[20];
+
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int T, P;
+		cin >> T >> P;
+
+		consul[i][0] = T;
+		consul[i][1] = P;
+	}
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		if ((consul[i][0] + i) > N)
+		{
+			dp[i] = dp[i + 1];
+		}
+		else
+		{
+			dp[i] = max((consul[i][1] + dp[i + consul[i][0]]), dp[i]);
+			dp[i] = max(dp[i], dp[i + 1]);
+			sum = dp[i];
+		}
+	}
+
+	cout << sum;
+
+}
