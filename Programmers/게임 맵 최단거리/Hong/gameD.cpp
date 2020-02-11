e6724efd99af87e7f1f2de067c13b4f9d59b1b91
jonghyunH <adad357@naver.com>
2020-02-11 19:01:34 +0900

    solve #6 게임 맵 최단거리

---
+++ Programmers/게임 맵 최단거리/Hong/gameD.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int visit[105][105];
+
+struct node
+{
+	int x;
+	int y;
+	int d;
+};
+
+int bfs(vector<vector<int> > maps)
+{
+	int dx[4] = { -1,0,1,0 };
+	int dy[4] = { 0,1,0,-1 };
+	int n = maps.size();
+	int m = maps[0].size();
+	queue<node> q;
+
+	q.push({ 0,0,1 });
+	visit[0][0] = true;
+
+	while (!q.empty())
+	{
+		node val;
+		val = q.front();
+		q.pop();
+
+		if (val.x == n - 1 && val.y == m - 1)
+		{
+			return val.d;
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			int cx = val.x + dx[i];
+			int cy = val.y + dy[i];
+
+			if (cx >= 0 && cy >= 0 && cx < n && cy < m && !visit[cx][cy] && maps[cx][cy] == 1)
+			{
+				q.push({ cx,cy,val.d+1 });
+				visit[cx][cy] = true;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int solution(vector<vector<int> > maps)
+{
+	int answer = 0;
+
+	answer = bfs(maps);
+	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> maps =
+	{
+		{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}
+	};
+
+	cout << solution(maps);
+}
+
