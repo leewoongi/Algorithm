65ebeebc1b1042c3b257744ea9f216423bba3a51
jonghyunH <adad357@naver.com>
2020-03-04 12:33:15 +0900

    solve #7 boj_4796

---
+++ BackJoon/4796/Hong/cam.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int L, P, V;
+	int ca = 1;
+
+	cin >> L >> P >> V;
+
+	while (L != 0 && P != 0 && V != 0)
+	{
+		int c = V / P;
+		int a = V % P;
+		
+		if (a > L)
+		{
+			a = L;
+		}
+		int result = (c * L) + a;
+
+		cout << "Case " << ca << ": " << result << '\n';
+		ca++;
+
+		cin >> L >> P >> V;
+	}
+}
