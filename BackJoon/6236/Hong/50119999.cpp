2b223c4bb56f778f842704337595e9f5a4c2830d
jonghyunH <adad357@naver.com>
2020-03-10 18:39:26 +0900

    solve #8 boj_6236

---
+++ BackJoon/6236/Hong/50119999.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, M, counter, result = 2100000000;
+int mo[100005];
+int Left = 210000000;
+int Right;
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> mo[i];
+		Left = min(Left, mo[i]);
+		Right = Right + mo[i];
+	}
+
+	while (Left <= Right)
+	{
+		bool check = true;
+		counter = 0;
+		int mid = (Left + Right) / 2;
+		int val = 0;
+		for (int i = 0; i < N; i++)
+		{
+
+			if (mid < mo[i])
+			{
+				check = false;
+				break;
+			}
+			else {
+
+				if (val >= mo[i])
+				{
+					val = val - mo[i];
+				}
+				else
+				{
+
+					counter++;
+					val = mid;
+					val = val - mo[i];
+				}
+			}
+		}
+
+		if (M < counter || !check)
+		{
+			Left = mid + 1;
+		}
+		else
+		{
+
+			if (result > mid)
+			{
+				result = mid;
+			}
+			Right = mid - 1;
+		}
+	}
+	cout << result;
+}
