d3e94ee2ff8af9e0a1a52694abd36944a868fe37
jonghyunH <adad357@naver.com>
2020-03-12 17:04:17 +0900

    solve #8 boj_2512

---
+++ BackJoon/2512/Hong/money.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N, M, Left, Right, Mid, add;
+vector<int> v;
+
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		v.push_back(a);
+		Right = max(a, Right);
+	}
+	cin >> M;
+
+	while (Left <= Right)
+	{
+		Mid = (Left + Right) / 2;
+		add = 0;
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (v[i] >= Mid)
+			{
+				add += Mid;
+			}
+			else
+			{
+				add += v[i];
+			}
+		}
+
+		if (add > M)
+		{
+			Right = Mid - 1;
+		}
+		else
+		{
+			Left = Mid + 1;
+		}
+	}
+
+	cout << Right;
+}
