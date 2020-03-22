b517a7e74ee5d58aff40486ed8edb92c5f21d8be
jonghyunH <adad357@naver.com>
2020-03-22 23:31:47 +0900

    solve #10 완주하지못한선수

---
+++ Programmers/완주하지 못한 선수/Hong/comp.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<string>
+#include<set>
+#include<map>
+#include<cmath>
+#include<stack>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion)
+{
+	string answer = "";
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+
+	for (int i = 0; i < completion.size(); i++)
+	{
+		if (participant[i] == completion[i])
+			continue;
+		else
+			return participant[i];
+	}
+
+	return participant[participant.size() - 1];
+}
+
+int main()
+{
+	vector<string> a = { {"leo"}, {"kiki"}, {"eden"} };
+	vector<string> b = { {"kiki"}, {"eden"} };
+
+	solution(a, b);
+}
