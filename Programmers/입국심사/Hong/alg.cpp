ba6067f61ec9c593e4138c29fdf763a07058a6a2
jonghyunH <adad357@naver.com>
2020-04-10 18:14:17 +0900

    solve #11 입국심사

---
+++ Programmers/입국심사/Hong/alg.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<string>
+#include<set>
+#include<map>
+#include<cmath>
+#include<stack>
+#include<utility>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    long long left = 1;
+    long long right = 999999999999999999;
+    long long mid;
+
+    while (left <= right)
+    {
+        long long counter = 0;
+        mid = (left + right) / 2;
+        for (int i = 0; i < times.size(); i++)
+        {
+            counter += mid / times[i];
+        }
+
+        if (counter >= n)
+        {
+            right = mid - 1;
+        }
+        else
+            left = mid + 1;
+    }
+
+    answer = left;
+
+    return answer;
+}
+
+int main()
+{
+    int n = 2;
+    vector<int> times = { 1,1 };
+
+    cout << solution(n, times);
+}
